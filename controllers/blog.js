a51ebdc4e17cc9fa60e8da616883a61e3aaa6486
Nagendra <nagendrababu.marasu@gmail.com>
2024-01-30 18:09:36 +0530

    part4 exercises completed

---
+++ controllers/blog.js
@@ -0,0 +1,88 @@
+const blogRouter = require('express').Router();
+const Blog = require('../models/blog');
+const User = require('../models/user');
+const jwt = require('jsonwebtoken');
+const middleware = require('../utils/middleware');
+
+blogRouter.get('/', async (request, response) => {
+	const blogs = await Blog.find({});
+	response.json(blogs);
+});
+
+blogRouter.post(
+	'/',
+	middleware.userExtractor,
+	async (request, response) => {
+		const body = request.body;
+		const decodedToken = jwt.verify(
+			request.token,
+			process.env.SECRET
+		);
+		if (!decodedToken.id) {
+			return response
+				.status(401)
+				.json({ error: 'token invalid' });
+		}
+		const user = request.user;
+
+		if (!body.title || !body.url) {
+			return response.status(400).json({
+				error: 'missing blog title or blog url',
+			});
+		}
+
+		const newBlog = {
+			title: request.body.title,
+			author: request.body.author,
+			url: request.body.url,
+			likes: request.body.likes || 0,
+			user: user.id,
+		};
+		const blog = new Blog(newBlog);
+		const savedBlog = await blog.save();
+		user.blogs = user.blogs.concat(savedBlog._id);
+		await user.save();
+		response.status(201).json(savedBlog);
+	}
+);
+
+blogRouter.put('/:id', async (request, response) => {
+	const updatedBlog = await Blog.findByIdAndUpdate(
+		request.params.id,
+		request.body,
+		{ new: true }
+	);
+	response.json(updatedBlog);
+});
+
+blogRouter.delete(
+	'/:id',
+	middleware.userExtractor,
+	async (request, response) => {
+		const decodedToken = jwt.verify(
+			request.token,
+			process.env.SECRET
+		);
+		if (!decodedToken.id) {
+			return response
+				.status(401)
+				.json({ error: 'invalid token' });
+		}
+		const blog = await Blog.findById(request.params.id);
+		if (blog.user.toString() != decodedToken.id) {
+			return response.status(401).json({
+				error:
+					'you can`t delete this blog as you are not owner of the blog',
+			});
+		}
+		const user = request.user;
+		user.blogs = user.blogs.filter(
+			(blog) => blog.id !== request.params.id
+		);
+		await user.save();
+		await Blog.findByIdAndDelete(request.params.id);
+		response.status(204).end();
+	}
+);
+
+module.exports = blogRouter;
